84b38f2e58ced9946c4498369a3034b9dafb5c13
Kyrch <kyrchk5@gmail.com>
2023-04-30 02:04:34 -0300

    added line missing

--- batch_encoder/__main__.py
+++ batch_encoder/__main__.py
@@ -59,6 +59,7 @@ def main():
                               EncodingConfig.config_crfs: EncodingConfig.default_crfs,
                               EncodingConfig.config_threads: EncodingConfig.default_threads,
                               EncodingConfig.config_limit_size_enable: EncodingConfig.default_limit_size_enable,
+                              EncodingConfig.config_alternate_source_files: EncodingConfig.default_alternate_source_files,
                               EncodingConfig.config_include_unfiltered: EncodingConfig.default_include_unfiltered,
                               EncodingConfig.config_default_video_stream: '',
                               EncodingConfig.config_default_audio_stream: ''}
